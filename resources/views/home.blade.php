fdaa84ed17202d8be0ebdb5e2dfd4f3e4dce0ded
Lorenzo Pagani <larrypg2@gmail.com>
2024-01-22 18:13:38 +0100

    creates model and controller

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>Dati da config/data.js: {{ $home }}</h1>
-<p>This is my paragraph content.</p>
-<button class="btn btn-primary">Cliccami</button>
+    <main>
+        <div class="container">
+            <div class="row">
+                <div class="col-12 d-flex justify-content-between flex-wrap">
+                    @foreach ($movies as $movie)
+                        <div class="card m-3" style="width:calc(100% / 5 - 2rem);">
+                            <img src="https://w7.pngwing.com/pngs/749/268/png-transparent-generic-icon-icons-matt-movie-symbol-video-thumbnail.png"
+                                class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">{{ $movie->title }}</h5>
+                                <h6 class="card-subtitle mb-2 text-muted ">{{ $movie->original_title }}</h6>
+                                <p class="card-text">Vote: {{ $movie->vote }}</p>
+
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+            </div>
+        </div>
+    </main>
 @endsection
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,4 @@
-<header>
-    <a href="{{ route('home') }}">Home</a>
+<header class="text-center">
+    <h1>Movies</h1>
+    <p>A list of movies</p>
 </header>
